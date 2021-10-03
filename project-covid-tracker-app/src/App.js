d60662dfcf5cb75f8e2c0c5dda3988417e4c993a
jaffasunny <jaffer.sunny125@gmail.com>
2021-10-03 21:46:28 +0500

    Covid Tracker App: initial commit

---
+++ project-covid-tracker-app/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import InfoPanel from "./Components/InfoPanel";
+import Navbar from "./Components/Navbar";
+function App() {
+	return (
+		<div>
+			<Navbar />
+      <InfoPanel />
+		</div>
+	);
+}
+
+export default App;
---
+++ project-covid-tracker-app/src/Components/InfoPanel.js
@@ -0,0 +1,33 @@
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+
+const Item = styled(Paper)(({ theme }) => ({
+	...theme.typography.body2,
+	padding: theme.spacing(1),
+	textAlign: "center",
+	color: theme.palette.text.secondary,
+	maxWidth: 1000,
+	margin: "0 auto",
+	marginTop: 50,
+}));
+
+export default function InfoPanel() {
+	return (
+		<Box sx={{ flexGrow: 1 }}>
+			<Grid container spacing={2}>
+				<Grid item xs={12} sm={4}>
+					<Item sx={{ boxShadow: 3 }}>Grid 1</Item>
+				</Grid>
+				<Grid item xs={12} sm={4}>
+					<Item sx={{ boxShadow: 3 }}>Grid 2</Item>
+				</Grid>
+				<Grid item xs={12} sm={4}>
+					<Item sx={{ boxShadow: 3 }}>Grid 3</Item>
+				</Grid>
+			</Grid>
+		</Box>
+	);
+}
