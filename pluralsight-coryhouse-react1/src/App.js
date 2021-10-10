a9b8ad5ad9dd935a17470d59bbc0b51991f1f109
jaffasunny <jaffer.sunny125@gmail.com>
2021-10-10 19:20:33 +0500

    React Router: NavLink and Link added

--- pluralsight-coryhouse-react1/src/App.js
+++ pluralsight-coryhouse-react1/src/App.js
@@ -3,29 +3,15 @@ import AboutPage from "./components/AboutPage";
 import Header from "./components/common/Header";
 import CoursePage from "./components/CoursePage";
 import HomePage from "./components/HomePage";
+import { Route } from "react-router-dom";
 
 function App() {
-	function getPage() {
-		const route = window.location.pathname;
-		if (route === "/about")
-			return (
-				<div className="App">
-					<AboutPage />
-				</div>
-			);
-		if (route === "/courses")
-			return (
-				<div className="App">
-					<CoursePage />
-				</div>
-			);
-		return <HomePage />;
-	}
-
 	return (
 		<div className="container-fluid">
 			<Header />
-			{getPage()}
+			<Route path="/" exact component={HomePage} />
+			<Route path="/courses" component={CoursePage} />
+			<Route path="/about" component={AboutPage} />
 		</div>
 	);
 }
--- pluralsight-coryhouse-react1/src/components/HomePage.js
+++ pluralsight-coryhouse-react1/src/components/HomePage.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function HomePage() {
 	return (
 		<div class="h-100 p-5 bg-light border rounded-3">
 			<h1>Pluralsight Administration</h1>
 			<p>React, Flux and React Router for ultra-responsive web apps.</p>
-			<a href="/about">About</a>
+			<Link to="/about" className="btn btn-primary">
+				About
+			</Link>
 		</div>
 	);
 }
