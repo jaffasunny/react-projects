8c934ded503f6dfcb22aebad5450eb86b442274b
jaffasunny <jaffer.sunny125@gmail.com>
2021-10-10 20:06:56 +0500

    React Prompt used

--- pluralsight-coryhouse-react1/src/components/ManageCoursePage.js
+++ pluralsight-coryhouse-react1/src/components/ManageCoursePage.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { Prompt } from "react-router";
 
 const ManageCoursePage = (props) => {
 	console.log(props.match.params.slug);
 	return (
 		<>
 			<h2>Manage Course</h2>
+			{/* <Prompt when={true} message="Are you sure you want to leave?" /> */}
 			{props.match.params.slug}
 		</>
 	);
