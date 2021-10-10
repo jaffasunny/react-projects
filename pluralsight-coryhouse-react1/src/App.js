840712e0f177b97024a9e9ea1db4a67065189c8b
jaffasunny <jaffer.sunny125@gmail.com>
2021-10-10 19:34:51 +0500

    Switch implemented to inuse 404 Page

--- pluralsight-coryhouse-react1/src/App.js
+++ pluralsight-coryhouse-react1/src/App.js
@@ -3,15 +3,19 @@ import AboutPage from "./components/AboutPage";
 import Header from "./components/common/Header";
 import CoursePage from "./components/CoursePage";
 import HomePage from "./components/HomePage";
-import { Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
+import NotFoundPage from "./components/NotFoundPage";
 
 function App() {
 	return (
 		<div className="container-fluid">
 			<Header />
-			<Route path="/" exact component={HomePage} />
-			<Route path="/courses" component={CoursePage} />
-			<Route path="/about" component={AboutPage} />
+			<Switch>
+				<Route path="/" exact component={HomePage} />
+				<Route path="/courses" component={CoursePage} />
+				<Route path="/about" component={AboutPage} />
+				<Route component={NotFoundPage} />
+			</Switch>
 		</div>
 	);
 }
